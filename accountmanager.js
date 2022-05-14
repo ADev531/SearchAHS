1e7abe234621d509e434f7de7e96dc85a34f530d
A Dev <82761748+ADev531@users.noreply.github.com>
2022-05-14 15:33:18 +0900

    Update accountmanager.js

--- accountmanager.js
+++ accountmanager.js
@@ -1,4 +1,19 @@
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-analytics.js";
+
+const firebaseConfig = {
+   apiKey: "AIzaSyCxwXejj3zuSz9FRK43EJBfWYzWcIkQLWU",
+   authDomain: "cloudbrowser-ecda5.firebaseapp.com",
+   projectId: "cloudbrowser-ecda5",
+   storageBucket: "cloudbrowser-ecda5.appspot.com",
+   messagingSenderId: "377912068769",
+   appId: "1:377912068769:web:dedc98dd783579d46cfee0",
+   measurementId: "G-FHQZSTKQ66"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const loginbutton = document.getElementById("loginbutton");
 
